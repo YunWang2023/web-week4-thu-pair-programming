6fd124a8d19b79731cbbc3b5da2b91ec82f7f01a
Georgs Grants <georgs.grants@metropolia.fi>
2026-09-09 15:11:45 +0300

    feat(components) : add removing services functionality

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,4 +1,5 @@
-const Service = ({ icon, title, text, details }) => {
+const Service = ({ id, icon, title, text, details, onRemove }) => {
+
   return (
     <article className='service'>
       <span className='service-icon'>
@@ -9,6 +10,9 @@ const Service = ({ icon, title, text, details }) => {
         <p className='service-text'>{text}</p>
         {details && <p className='service-details'>{details}</p>}
       </div>
+      <button onClick={() => onRemove(id)}>
+        Remove
+      </button>
     </article>
   )
 }
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -6,13 +6,17 @@ import Service from './Service';
 const Services = () => {
   const [servicesData, setServicesData] = useState(services);
 
+  const removeService = (serviceId) => {
+    setServicesData(servicesData.filter(service => service.id !== serviceId));
+  }
+
   return (
     <section className='section services' id='services'>
       <Title title='our' subtitle='services' />
 
       <div className='section-center services-center'>
         {servicesData.map((service) => {
-          return <Service {...service} key={service.id} />;
+          return <Service {...service} key={service.id} onRemove={removeService}/>;
         })}
       </div>
     </section>
